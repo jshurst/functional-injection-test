837d2fbf9e3220f4e1a8e429efc39b504cda8e37
jshurst <hurst1109@yahoo.com>
2015-01-10 15:34:01 -0500

    initial commit

---
+++ FunctionalInjection.Web/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FunctionalInjection.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Func<string, string> _getPerson;
+        private Func<string, string> _getAddress;
+
+        public HomeController(Func<string, string> getPerson, Func<string, string> getAddress)
+        {
+            _getPerson = getPerson;
+            _getAddress = getAddress;
+        }
+
+        public ActionResult Index()
+        {
+            var personId = "1";
+            ViewBag.Person = _getPerson(personId);
+            ViewBag.Address = _getAddress(personId);
+            return View();
+        }
+    }
+}
---
+++ FunctionalInjection.Web/DataRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalInjection.Web
+{
+    public class DataRepo
+    {
+        public static string GetPerson(string personId)
+        {
+            return "Found person " + personId;
+        }
+
+        public static string GetAddress(string personId)
+        {
+            return "Found city for person " + personId;
+        }
+    }
+}
